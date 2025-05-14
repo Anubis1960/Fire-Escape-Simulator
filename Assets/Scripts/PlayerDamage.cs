00919c8cc31dc20f28718b041ded7a36147bf808
Catalin <florin.lazar.catalin02@gmail.com>
2025-05-14 18:04:37 +0300

    Add oxygen bar with oxygen loss on diferent scenarios as on idle, jumping or running

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
@@ -7,13 +8,26 @@ public class PlayerDamage : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    public int maxOxygen = 100;
+    public int currentOxygen;
+
     public HealthBar healthBar;
+    public OxygenBar oxygenBar;
+       
+    private Coroutine oxygenCoroutine;
+    private Movement movementScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        movementScript = GetComponent<Movement>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+
+        currentOxygen = maxOxygen;
+        oxygenBar.SetMaxOxygen(maxOxygen);
+
+        oxygenCoroutine = StartCoroutine(Breathe());
     }
 
     // Update is called once per frame
@@ -35,5 +49,34 @@ public class PlayerDamage : MonoBehaviour
             Debug.Log("Player is dead.");
             // TODO
         }
+    }
+
+    public void ReduceOxygen(int oxygenLoss)
+    {
+        currentOxygen -= oxygenLoss;
+        oxygenBar.SetOxygen(currentOxygen);
+    }
+
+    IEnumerator Breathe()
+    {
+        while (currentOxygen > 0)
+        {
+            float waitTime = 1f;
+            int oxygenLoss = 1;
+
+            // Two times more oxygen loss while sprinting
+            if (movementScript.IsSprinting)
+            {
+                waitTime = 0.5f;
+                oxygenLoss = 2;
+            }
+
+            yield return new WaitForSeconds(waitTime);
+            currentOxygen -= oxygenLoss;
+            oxygenBar.SetOxygen(currentOxygen);
+        }
+
+        Debug.Log("Out of oxygen!");
+        // TODO
     }
 }
